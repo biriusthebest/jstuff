ff3edb22335d8c596831d0dd1bf8bf38b355401b
Bohdan Palekha <palekha.b@gmail.com>
2026-01-22 05:11:45 +0100

    RED: Add a failing test for the isValidEmail method

---
+++ email-validator/src/main/java/org/author/demo/EmailValidator.java
@@ -0,0 +1,11 @@
+package org.author.demo;
+
+package org.author.demo;
+
+public class EmailValidator {
+
+    public boolean isValidEmail(String email) {
+        // TODO: Implement the Email validation logic
+        return false;
+    }
+}
--- email-validator/src/test/java/org/author/demo/EmailValidatorTest.java
+++ email-validator/src/test/java/org/author/demo/EmailValidatorTest.java
@@ -1,4 +1,14 @@
 package org.author.demo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class EmailValidatorTest {
+
+    @Test
+    public void testIsValidEmail_ValidEmail_ReturnsTrue() {
+        EmailValidator emailValidator = new EmailValidator();
+        boolean result = emailValidator.isValidEmail("test@example.com");
+        Assertions.assertTrue(result);
+    }
 }
