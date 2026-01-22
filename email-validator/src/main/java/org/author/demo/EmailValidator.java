53fbf0d17ab010ed8dc48ba4704c616ba4410778
Bohdan Palekha <palekha.b@gmail.com>
2026-01-22 05:13:50 +0100

    git commit -m "GREEN: Implement the isValidEmail method to make the test pass"

--- email-validator/src/main/java/org/author/demo/EmailValidator.java
+++ email-validator/src/main/java/org/author/demo/EmailValidator.java
@@ -1,11 +1,13 @@
 package org.author.demo;
 
-package org.author.demo;
-
 public class EmailValidator {
 
     public boolean isValidEmail(String email) {
-        // TODO: Implement the Email validation logic
-        return false;
+        // Simple validation for demonstration purposes
+        if (email.contains("@")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
